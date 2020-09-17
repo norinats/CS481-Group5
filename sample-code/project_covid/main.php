b0c2f65e698505a2cfecead99158bffce1568265
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2020-09-16 22:49:28 -0600

    US map demo added

---
+++ sample-code/project_covid/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE php>
+<html>
+<head>
+    <title>COVID TRACKER mainpage</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>COVID TRACKER
+            <hr>
+        </h1>
+    </header>
+    <main>
+    <div id="content">
+        <div id="lobbytitle">
+            <h2>US Map demo</h2>
+            <p>50 states and their COVID stats:</p><br>
+        </div>
+        <div id="lobbyform">
+            <form id="createGame" method="POST" action="game.php">
+              <script src="mapdata.js"></script>
+              <script src="usmap.js"></script>
+        			<div id="map"></div>
+            </form>
+        </div>
+    </div>
+    </main>
