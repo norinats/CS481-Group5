b2487f16b317951f8002230c2ea053d499160b57
Nori Natsuhara <norinatsuhara@u.boisestate.edu>
2020-09-24 08:53:38 -0600

    changed name of homepage to index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE php>
+<html>
+<head>
+    <title>COVID TRACKER mainpage</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <ul>
+        <li><h1>COVID TRACKER</h1></li>
+        <li><a class="active" href="main.php">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#covidinfo">COVID-19 Info</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </header>
+    <main>
+    <div id="content">
+        <script src="mapdata.js"></script>
+        <script src="usmap.js"></script>
+        <div id="map"></div>
+    </div>
+    </main>
