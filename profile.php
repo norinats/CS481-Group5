19f1cb2b1c3343b429bb78b238fa0491f84bbe83
Nori Natsuhara <norinatsuhara@u.boisestate.edu>
2020-09-29 13:21:29 -0600

    added profile.css and profile.php

---
+++ profile.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="profile.css">
+    </head>
+
+    <body>
+    <header>
+        <ul>
+        <li><h1>COVID TRACKER</h1></li>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#covidinfo">COVID-19 Info</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </header>
+
+    
+    </body>
+</html>
