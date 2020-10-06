f8cf96332a3cdf32283e598e624ac1b871b44282
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2020-10-06 13:32:54 -0600

    Add description(jquery) and sticky navbar

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
 <!DOCTYPE php>
 <html>
 <head>
-    <title>COVID TRACKER mainpage</title>
+    <title>COVID TRACKER Homepage</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
-        <ul>
+        <div id="navbar">
         <li><h1>COVID TRACKER</h1></li>
         <li><a class="active" href="index.php">Home</a></li>
         <li><a href="#news">News</a></li>
         <li><a href="#covidinfo">COVID-19 Info</a></li>
         <li><a href="#about">About</a></li>
-      </ul>
+      </div>
     </header>
     <main>
     <div id="content">
-        <script src="mapdata.js"></script>
-        <script src="usmap.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/mapdata.js"></script>
+        <script src="js/usmap.js"></script>
+        <script src="js/update_map.js"></script>
         <div id="map"></div>
     </div>
     </main>
+
+
+
+<!-- MAKE NAVBAR STICKY -->
+    <script>
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+    </script>
+
+</body>
+</html>
--- profile.php
+++ profile.php
@@ -1,19 +1,29 @@
+<!doctype html>
 <html>
-    <head>
-        <link rel="stylesheet" href="profile.css">
-    </head>
+<head>
+    <title>?????</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<title><?php echo $_GET['title']; ?></title>
 
-    <body>
-    <header>
-        <ul>
-        <li><h1>COVID TRACKER</h1></li>
-        <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#covidinfo">COVID-19 Info</a></li>
-        <li><a href="#about">About</a></li>
-      </ul>
-    </header>
-
-    
-    </body>
+<body>
+  <header>
+    <div id="navbar">
+    <li><h1>COVID TRACKER</h1></li>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#covidinfo">COVID-19 Info</a></li>
+    <li><a href="#about">About</a></li>
+  </div>
+  </header>
+  <a type="button" href="index.php">Back</a>
+<div class="search-div">
+<?php echo "<img src={$_GET['imgsrc']}>"; ?>
+<h2><?php echo $_GET['title']; ?></h2>
+<strong>Overview: </strong><?php echo $_GET['plot']; ?><br>
+<strong>Actors/Actresses: </strong><?php echo $_GET['actors']; ?>
+</div>
+</body>
 </html>
+
+<!-- We used $_GET to get the information we previously established on another page $_GET['variableName'] -->
