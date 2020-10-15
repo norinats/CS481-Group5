a22a526a8f9d02eaf52a3fc099c6122b5750fa65
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2020-10-15 14:15:38 -0600

    fix stickynavbar + add searchbar

--- index.php
+++ index.php
@@ -20,16 +20,25 @@
         <script src="js/mapdata.js"></script>
         <script src="js/usmap.js"></script>
         <script type="text/javascript" src="js/select.js"></script>
+        
+        <script type='text/javascript' src='http://code.jquery.com/jquery-1.11.1.min.js'></script>
+        <link rel="stylesheet" href="css/chosen.css">
+        <script type='text/javascript' src='js/chosen.jquery.min.js'></script>
+        <select data-placeholder="Search for State..." id="state_list" style="width:350px;" tabindex="1">
+          <option value=""></option>
+        </select>
+        <script src="js/search.js"></script>
+        
         <script src="js/update_map.js"></script>
         <div id="map"></div>
     </div>
     </main>
 
-
+    
+<script>resetCookies();</script>
 
 <!-- MAKE NAVBAR STICKY -->
     <script>
-    resetCookies();
     window.onscroll = function() {myFunction()};
 
     var navbar = document.getElementById("navbar");
