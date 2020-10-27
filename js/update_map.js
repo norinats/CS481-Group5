d05faedd61b07545193c9ab50322dd8cc5638080
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2020-10-27 14:05:15 -0600

    Implemented heatmap in update_map.js

--- js/update_map.js
+++ js/update_map.js
@@ -9,9 +9,35 @@ jQuery(document).ready(function(){
         if (map_state) {
           map_state['description'] = "Total case: " + result[index]["positive"]
           + "<div>Death: " + result[index]["death"] + "</div>";
+          
+          var cpercent = (60 - result[index]["positive"]/13000);
+          if (cpercent > 0) {
+            map_state['color'] = increase_brightness('#0071a4', cpercent);
+          } else {
+            map_state['color'] = increase_brightness('#0071a4', cpercent + 9.5);
+          }
         }
       })
       simplemaps_usmap.load();
     }
   });
 })
+
+function increase_brightness(hex, percent){
+    // strip the leading # if it's there
+    hex = hex.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(hex.length == 3){
+        hex = hex.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(hex.substr(0, 2), 16),
+        g = parseInt(hex.substr(2, 2), 16),
+        b = parseInt(hex.substr(4, 2), 16);
+
+    return '#' +
+       ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substr(1);
+}
