f0cda26519e53c3f2a6b660358249662342941c6
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2020-11-05 13:22:32 -0700

    Added about.php

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE php>
+<html>
+<head>
+    <title>COVID TRACKER About</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div id="navbar">
+        <li><h1>COVID TRACKER</h1></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#covidinfo">COVID-19 Info</a></li>
+        <li><a class="active" href="about.php">About</a></li>
+      </div>
+    </header>
+    <main>
+    <div>
+      <h1>About</h1>
+      <br>
+      <h2>Who made this website?</h2>
+      <h3>This site was created by Nori Natsuhara, Omar Gonzalez and Lam Nguyen from CS481 Group5 of
+        Boise State University.</h3>
+      <br><br>
+      <h2>Sources</h2>
+      <h3><u>https://covidtracking.com/data/api</h3>
+      <h3>https://simplemaps.com/us</u></h3>
+    </div>
+    </main>
+
+
+<!-- MAKE NAVBAR STICKY -->
+    <script>
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+    </script>
+
+</body>
+</html>
